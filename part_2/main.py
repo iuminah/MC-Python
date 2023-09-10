53097bb7c99eb38376dd003aef58848dd1a0b495
Minah <huytq.minah@gmail.com>
2023-09-10 17:06:54 +0700

    update select program

--- part_2/main.py
+++ part_2/main.py
@@ -1,5 +1,4 @@
-import re
-from ulti import checkContinue, getRange
+from ulti import checkContinue, getRange, checkParterm, isLeapYear
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
@@ -29,7 +28,7 @@ def findNumbersWithConditional():
             f"1./ Các số chia hết cho 7 và không chia hết cho 5 trong đoạn '{startValue}' và '{endValue}' là : " + ", ".join(
                 map(str, result)),
         )
-        print(f"Tổng số : {len(result)}")
+    print(f"Tổng số : {len(result)}")
 
 
 def findMuliples():
@@ -56,5 +55,66 @@ def findMuliples():
         print(obj)
 
 
-findNumbersWithConditional()
-findMuliples()
+def caclAge():
+    curentYear = int(datetime.now().strftime("%Y"))
+    inputDob = input(
+        "Nhập ngày, tháng, năm sinh theo định dạng (yyyy/mm/dd) : ")
+    userDob = int(inputDob[:4])
+
+    validateInput = checkParterm(inputDob)
+
+    if validateInput:
+        age = curentYear - userDob
+        print(f"3./ Tuổi của người này là : {age}", end=". ")
+        isLeapYear(userDob)
+    else:
+        check = input(
+            "!-- Chưa nhập đúng định dạng, Bạn muốn thử lại không (y/n) : ")
+        if check == "y" or check == "Y":
+            caclAge()
+
+
+def sortAlphabet():
+    inputString = input("Nhập một chuỗi các từ tách biệt bởi khoảng trắng : ")
+
+    arrString = ["dab, bc, ab, ab"]
+
+    arrString.sort()
+
+    print(arrString)
+
+
+def allProgram():
+    findNumbersWithConditional()
+    findMuliples()
+    caclAge()
+    # sortAlphabet()
+
+
+def showListProgram():
+    print("1 : Tìm tất cả các số chia hết cho 7 nhưng không phải bội số của 5, nằm trong đoạn")
+    print("2 : Tạo ra 1 chuỗi là bội số của chúng và in ra kết quả dạng list")
+    print("3 : Tính tuổi từ ngày tháng năm sinh và cho biết năm sinh có phải năm nhuận không")
+    print("0 : Chạy lần lượt từng chương trình")
+
+
+def selectProgram(selected):
+    switcher = {
+        1: findNumbersWithConditional,
+        2: findMuliples,
+        3: caclAge,
+        0: allProgram
+    }
+    switcher.get((selected), lambda: print("Done"))()
+
+    rerun = input("Bạn có muốn chạy chương trình khác không (y/n) : ")
+    if rerun == "y" or rerun == "Y":
+        selectAgain = int(input("Chọn chương trình bạn muốn chạy : "))
+        showListProgram()
+        selectProgram(selectAgain)
+
+
+if __name__ == "__main__":
+    showListProgram()
+    selected = int(input("Chọn chương trình bạn muốn chạy : "))
+    selectProgram(selected)
--- part_2/ulti.py
+++ part_2/ulti.py
@@ -1,3 +1,5 @@
+import re
+
 
 def checkContinue():
     check = input(
@@ -14,3 +16,15 @@ def getRange():
             "Nhập giá trị bắt đầu và kết thúc để tạo dẫy số (Hai giá trị được phân biệt bởi dấu cách): ").split()
     ]
     return startValue, endValue
+
+
+def isLeapYear(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        print(f'Năm "{year}" Có phải là năm nhuận')
+    else:
+        print(f'Năm "{year}" Không phải là năm nhuận')
+
+
+def checkParterm(str):
+    parterm = re.match(r"\d{4}/\d{2}/\d{2}", str)
+    return bool(parterm)
